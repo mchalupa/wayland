f7966db57eaa1788252f504f99d0589f42e6474c
Marek Ch <mchqwerty@gmail.com>
2013-11-05 12:26:36 +0100

    test-compositor: add main header file
    Add test-compositor header file

---
+++ tests/test-compositor/test-compositor.h
@@ -0,0 +1,9 @@
+#ifndef __TEST_COMPOSITOR_H__
+#define __TEST_COMPOSITOR_H__
+
+#include "test-compositor/tc-server.h"
+#include "test-compositor/tc-client.h"
+#include "test-compositor/tc-events.h"
+#include "test-compositor/tc-config.h"
+
+#endif /* __TEST_COMPOSITOR_H__ */
